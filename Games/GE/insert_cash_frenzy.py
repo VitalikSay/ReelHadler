609a98de9438560426297a0be68deda557c2affd
Vitaliy Sayganov <vitalij.saiganov@1pt.com>
2024-06-07 18:37:26 +0300

    ged add

--- Games/GE/insert_cash_frenzy.py
+++ Games/GE/insert_cash_frenzy.py
@@ -4,7 +4,8 @@ import xml.etree.ElementTree as ET
 import numpy as np
 
 handler = BasicPathHandler()
-reels_path = handler.GetResultDataFilePath('GE', 'frenzy_4.xml', 'Frenzy_reels_source')
+file_name = 'frenzy_all.xml'
+reels_path = handler.GetResultDataFilePath('GE', file_name, 'Frenzy_reels_source')
 print(reels_path)
 settings_xml = ET.parse(reels_path).getroot()
 
@@ -14,8 +15,10 @@ settings.ReadSettings_Xml(settings_xml)
 reelsets = settings.GetReelsets()
 
 cf_symbols = np.array([11, 12, 13, 14, 15, 16, 17, 18, 19])
-cf_symbols_count = np.array([15, 8, 6, 3, 3, 2, 1, 1, 1])
-# cf_symbols_count = np.array([15, 9, 7, 3, 2, 1, 1, 1, 1])
+cf_symbols_count = np.array([12, 9, 7, 4, 3, 2, 1, 1, 1]) # 3 reel
+# cf_symbols_count = np.array([15, 9, 7, 3, 2, 1, 1, 1, 1]) # 2,4 reels
+# cf_symbols_count = np.array([20, 6, 5, 3, 2, 1, 1, 1, 1]) # 1,5, all reels
+
 
 tmp = [[cf_symbols[i] for _ in range(cf_symbols_count[i])] for i in range(len(cf_symbols))]
 all_symbols = []
@@ -38,5 +41,5 @@ for reelset in reelsets:
             if symbol == temp_symb:
                 reel.symbols[i] = all_symbols[temp_indx]
                 temp_indx += 1
-settings.SaveSettings('GE', 'frenzy_4.xml', "Frenzy_reels_processed")
+settings.SaveSettings('GE', file_name, "Frenzy_reels_processed")
 
